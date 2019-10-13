daaec1b7fc900bc950fa2b7d0ae706e2513dddec
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-13 20:52:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,7 +31,7 @@ def reduce_to_all_true(source_array)
     return false if (source_array[counter] = false)
   counter += 1 
   end
-  return false
+  return true
 end
     
 def reduce_to_any_true(source_array)
@@ -40,5 +40,5 @@ def reduce_to_any_true(source_array)
    return true if (source_array[counter] = true)
    counter += 1 
  end
- return true
+ return false
 end
